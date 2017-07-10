f827d03dffd8fbb3c7f068914dab2b7e6b88878c
Vlad Z <vladislavz1991@gmail.com>
2017-07-10 18:10:55 -0400

    TextFormInputNode setup

---
+++ Clark/UI Components/Nodes/Sections/Form Inputs/Text/TextFormInputNode.swift
@@ -0,0 +1,59 @@
+//
+//  TextFormInputNode.swift
+//  Clark
+//
+//  Created by Vladislav Zagorodnyuk on 7/10/17.
+//  Copyright © 2017 Clark. All rights reserved.
+//
+
+import UIKit
+import Kingfisher
+import AsyncDisplayKit
+
+class TextFormInputNode: ASCellNode {
+    
+    let textField = ASEditableTextNode()
+    
+    /// Model
+    var formInput: FormTextInput
+    
+    init(with formInput: FormTextInput) {
+        
+        /// Assign model
+        self.formInput = formInput
+        
+        super.init()
+        
+        /// Form input setup
+        
+        /// Placeholder
+        textField.attributedPlaceholderText = NSAttributedString(string: formInput.displayName, attributes: [NSFontAttributeName: UIFont.AvenirNextRegular(size: 17), NSForegroundColorAttributeName: UIColor.trinidad])
+        
+        /// Text color
+        textField.tintColor = UIColor.trinidad
+        textField.textView.textColor = UIColor.shipGray
+        textField.keyboardType = formInput.keyboardType
+        textField.autocapitalizationType = formInput.capitalizationType
+        
+        /// Delegate
+        textField.delegate = self
+        
+        /// Add subnode
+        addSubnode(textField)
+    }
+    
+    open override func layoutSpecThatFits(_ constrainedSize: ASSizeRange) -> ASLayoutSpec {
+        
+        let carouselMessageSize = ASAbsoluteLayoutSpec()
+        
+        carouselMessageSize.style.preferredSize = CGSize(width: constrainedSize.max.width, height: 45)
+        carouselMessageSize.sizing = .sizeToFit
+        carouselMessageSize.children = [textField]
+        
+        return ASInsetLayoutSpec(insets: .zero, child: carouselMessageSize)
+    }
+}
+
+extension TextFormInputNode: ASEditableTextNodeDelegate {
+    
+}
